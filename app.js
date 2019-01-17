90cc41ba7132459fb7000259f6455efa79e49e1f
Mstapleton22 <mike.stapleton2@gmail.com>
2019-01-17 10:50:51 -0700

    data shows on browser in nodemon

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3003
+const data = require('./data.json')
 
-const data = require('data.json')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.use(express.static('public')) // look it up
+
+app.get('/data', (req, res, next) => {
+  res.status(200).send({
+    "message": "success!",
+    "data": data
+  })
+})
+
+app.get("/:tag", (req, res, next) => {
+  res.status(200).send(req.params.tag)
+})
+
+app.listen(port, () => console.log(`party on port ${port}`))
